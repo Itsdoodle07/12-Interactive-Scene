05fee958d8af748245b4dda4c319d3f027432964
Itsdoodle07 <pancakeieat1@gmail.com>
2026-09-24 15:15:29 -0700

    imgs

--- sketch.js
+++ sketch.js
@@ -6,13 +6,20 @@
 // - describe what you did to take this project "above and beyond"
 
 // varibles
+
+
+// time varibles
 let milseconds = 0
 let seconds = 0
 let minutes = 0
-let stopOrstart = 1
-
-
-
+// attacks
+let attack1 = true
+let attack2 = true
+// health
+let health = 100
+// kill
+let death = false
+let evilguydeath = false
 // array's
 
 
@@ -20,7 +27,7 @@ let stopOrstart = 1
 
 function setup() {
   //This function get run once at the start of the program
-  createCanvas(600, 400);
+  createCanvas(1300, 800);
   background(240);
   // ellipseMode(CORNER);
   ellipseMode(CENTER);
@@ -56,44 +63,51 @@ function health(){
 
 // we will MAYBE use this for like trees and stuff
 function back_lanscape(x,y){
-
+  noStroke()
+  fill('gray')
+rect(x,y,1300,700)
+  fill ('black')
+rect(x,y-300,1300,100)
+rect(x,y+450,1300,400)
 }
 
 
 // this is will be our cooldown for weapons and or timed events
 function timeORcooldown(){
   // will we use this for time
+
   // base for time
+  milseconds = frameCount
   if (frameCount == 60){
 frameCount = 0
-milseconds += stopOrstart
   }
-
-
-
-
+  // resets milseconds
   if (milseconds == 60){
-    seconds += stopOrstart
     milseconds = 0
+    seconds += 1
   }
-  
+  // resets seconds
   if (seconds == 60){
-    minutes += stopOrstart
     seconds = 0
+    minutes += 1
   }
+
+
+  // shows time
 text(milseconds, 50, 50);
 text(seconds,100,50)
 text(minutes,200,50)
-if (keyIsDown(82)){
-stopOrstart = 0
-}
 }
 
 
 
-
+// where we draw stuff
 function draw() {
   background(220);
   timeORcooldown()
+  back_lanscape(650,300)
 }
-// 67
+// key code thing
+function keyPressed() {
+  console.log(keyCode)
+}
